52c8920d5579cf92dea265cc4eab6ab7475f34ae
Mahmoud Abuyoussef <mahmoudabuyoussef5@gmail.com>
2025-01-12 23:24:26 +0200

    Add: addTask & showTask & Storage Data in LocalStorage & show Data From Local Storage

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,3 +5,38 @@ document.getElementById("switch").onclick = () => {
     document.body.classList.add("dark-theme");
   }
 };
+
+/*
+ * In progress
+ * Completed
+ */
+
+/*
+ * localStorage.setItem("item", value)
+ * localStorage.getItem("item")
+ * localStorage.removeItem("item")
+ */
+
+let taskList = "";
+
+if (localStorage.getItem("tasks") !== null) {
+  taskList = localStorage.getItem("tasks");
+}
+
+function addTask() {
+  let taskInput = document.getElementById("task-input");
+  let taskStatus = "In progress";
+  taskList += `
+    <li>${taskInput.value}<br>
+    ${new Date().toLocaleString()}| ${taskStatus}</li>
+  `;
+  localStorage.setItem("tasks", taskList);
+  taskInput.value = "";
+  showTask();
+}
+
+function showTask() {
+  document.getElementById("task-list").innerHTML = localStorage.getItem("tasks");
+}
+showTask();
+document.getElementById("add-task-btn").onclick = () => addTask();
